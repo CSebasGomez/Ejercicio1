3df000e93941fca48911eec70e3cb5a56402fd21
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-03-01 16:02:54 -0500

    asdasd

---
+++ src/becker/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package becker;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class main {
+    public static void main(String[] args) {
+        Casa casita=new Casa();
+        casita.pintarCasa();
+    }
+    
+}
